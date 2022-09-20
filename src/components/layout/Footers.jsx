e2bbff847ab25dcb7feebbdf0bbaee551a6d4362
dahyun612@naver.com <dahyun612@naver.com>
2022-09-20 10:11:15 +0900

    푸터 아이콘배치 수정 및 CSS수정

--- src/components/layout/Footers.jsx
+++ src/components/layout/Footers.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./style.scss";
-import heartIcon from "../../asset/assetFooter/heartIcon.png";
-import writeIcon from "../../asset/assetFooter/writeIcon.png";
+import listIcon from "../../asset/assetFooter/listIcon.png";
+import recomendIcon from "../../asset/assetFooter/recomendIcon.png";
 import mypageIcon from "../../asset/assetFooter/mypageIcon.png";
+import addIcon from "../../asset/assetFooter/addIcon.png";
+import storyIcon from "../../asset/assetFooter/storyIcon.png";
+
 import { Navigate,useNavigate } from "react-router-dom";
 
 const Footers = () => {
@@ -11,8 +14,10 @@ const Footers = () => {
   return (
     <div className="Footer-Container">
       <div className="Icon-box">
-      <div className="btnbox"><img className="HeartIcon" onClick={() => { navigate("/mylike") }} src={heartIcon} /></div>
-      <div className="btnbox"><img className="writeIcon" onClick={() => { navigate("") }} src={writeIcon} /></div>
+      <div className="btnbox"><img className="recomendIcon" onClick={() => { navigate("/recommend") }} src={recomendIcon} /></div>
+      <div className="btnbox"><img className="listIcon" onClick={() => { navigate("/post") }} src={listIcon} /></div>
+      <div className="btnbox"><img className="addIcon" onClick={() => { navigate("") }} src={addIcon} /></div>
+      <div className="btnbox"><img className="storyIcon" onClick={() => { navigate("/story") }} src={storyIcon} /></div>
       <div className="btnbox"> <img className="mypageIcon" onClick={() => { navigate("/myinfo") }} src={mypageIcon} /></div>
     </div>
     </div>
