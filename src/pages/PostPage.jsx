8d3baa5ad26ce19bf98c495147ed49be833aaa4e
Eunji Roh <nonjee888@gmail.com>
2022-09-22 00:01:35 +0900

    yarn add buffer 했음, postlist 중복 import 지움

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,21 +3,18 @@ import Search from "../components/layout/Search";
 import Headers from "../components/layout/Headers";
 import PostList from "../components/post/PostList";
 import ListChanger from "../components/layout/ListChanger";
-import PostList from "../components/post/PostList";
 import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
 
 const PostPage = () => {
   return (
-      
-      <PageContainer>
-      <Headers/>
+    <PageContainer>
+      <Headers />
       <Search />
       <ListChanger />
-      <PostList/>
-      <Footers/>
-      </PageContainer>
-
+      <PostList />
+      <Footers />
+    </PageContainer>
   );
 };
 
