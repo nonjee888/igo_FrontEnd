89d28ae49e748ce8e260cabebd89a2bf9a7909f0
dahyun612@naver.com <dahyun612@naver.com>
2022-09-21 23:31:11 +0900

    페이지수정

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,22 +2,18 @@ import React from "react";
 import Search from "../components/layout/Search";
 import Headers from "../components/layout/Headers";
 import ListChanger from "../components/layout/ListChanger";
-import ScrollContainer from "../components/wrapper/ScrollContainer";
+import PostList from "../components/post/PostList";
 import Footers from "../components/layout/Footers";
-import Headerbar from "../components/layout/Headerbar";
 import PageContainer from "../components/wrapper/PageContainer";
 
 const PostPage = () => {
   return (
-    
-      
       
       <PageContainer>
       <Headers/>
-      
       <Search />
       <ListChanger />
-     
+      <PostList/>
       <Footers/>
       </PageContainer>
       
