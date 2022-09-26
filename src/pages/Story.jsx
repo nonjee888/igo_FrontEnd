86b3d06a88180adb9a19fd3fcb8709a9efd5f872
dahyun612@naver.com <dahyun612@naver.com>
2022-09-26 13:28:10 +0900

    css수정,카테고리 수정 및 스토리 페이지 이름변경

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
 import Headers from "../components/layout/Headers";
-import TravelStory from "../components/story/TravelStory";
+import VideoStory from "../components/story/VideoStory";
 
 
 const Story = () => {
   return(
     <PageContainer>
       <Headers/>
-      <TravelStory/>
+      <VideoStory/>
       <Footers/>
     </PageContainer>
     );
