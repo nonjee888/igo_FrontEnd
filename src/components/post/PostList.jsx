e3b10e1a7d479f8cc13bbf102a09f5985df8e37e
dahyun612@naver.com <dahyun612@naver.com>
2022-10-03 18:45:54 +0900

    category 포스트페이지에서 포스트컴포넌트로 위치변경 및 css수정

--- src/components/post/PostList.jsx
+++ src/components/post/PostList.jsx
@@ -1,10 +1,12 @@
 import "./style.scss";
 import React from "react";
 import Search from "../layout/Search";
+import Category from "../category/Category";
 
 const PostList = () => {
   return (
     <div className="All">
+      <Category/>
       <Search />
     </div>
   );
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,14 +3,12 @@ import Headers from "../components/layout/Headers";
 import PostList from "../components/post/PostList";
 import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
-import Category from "../components/category/Category";
 
 
 const PostPage = () => {
   return (
     <PageContainer>
       <Headers />
-      <Category/>
       <PostList />
       <Footers />
     </PageContainer>
