1aae9122c82ee6080dd3822bbf1379a9bfb79d31
Eunji Roh <nonjee888@gmail.com>
2022-10-04 18:39:34 +0900

    지도표시

--- src/components/post/PostDetail.jsx
+++ src/components/post/PostDetail.jsx
@@ -116,35 +116,18 @@ const PostDetail = () => {
             dangerouslySetInnerHTML={{ __html: detail?.content }}
           ></div>
           <div className="map-wrapper">
-            <Map // 지도를 표시할 Container
+            <Map
               center={{
                 // 지도의 중심좌표
                 lat: 33.450701,
                 lng: 126.570667,
               }}
               style={{
-                // 지도의 크기
                 width: "100%",
-                height: "200px",
+                height: "250px",
               }}
               level={3} // 지도의 확대 레벨
-            >
-              <Polyline
-                //  path={}
-                strokeWeight={5}
-                strokeColor={"#686EF9"}
-                strokeOpacity={1}
-                strokeStyle={"solid"}
-              />
-
-              <MapMarker
-              // position={{
-              //   lat: y,
-              //   lng: x,
-              // }}
-              // zIndex={zIndex}
-              />
-            </Map>
+            ></Map>
           </div>
         </div>
         <PostComment />
