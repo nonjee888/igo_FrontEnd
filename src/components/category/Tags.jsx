fb840caec33c79f1e1ef13c27f20412962a39b38
dahyun612@naver.com <dahyun612@naver.com>
2022-10-06 22:53:58 +0900

    아이콘 및 css색변경 (단색->그라데이션

--- src/components/category/Tags.jsx
+++ src/components/category/Tags.jsx
@@ -4,11 +4,11 @@ export default function Tags({selected,handler,name}) {
     return (
         <div
         className="tag"
-        style={selected ? {background:'#A3D7ED'} : {}}
+        style={selected ? {background:`linear-gradient(to left, #F5C9E0 30%,#47AFDB) 70%`} : {}}
         onClick={handler}>
 
             
-        <span className="tag-text" style={selected ? {color:'#000'} : {}}>{name}</span>
+        <span className="tag-text" style={selected ? {color:'#fff'} : {}}>{name}</span>
         </div>
     )
 }
