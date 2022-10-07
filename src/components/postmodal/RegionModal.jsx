33e09b37e0635b7567760f94bf99a7b03c6dd37f
dahyun612@naver.com <dahyun612@naver.com>
2022-10-07 13:07:34 +0900

    RegionModal 형식 수정-input->select형식으로 변경,수정예정

--- src/components/postmodal/RegionModal.jsx
+++ src/components/postmodal/RegionModal.jsx
@@ -1,5 +1,8 @@
-import React,{useState}from "react";
+import React,{useState, useEffect}from "react";
+import SelecTags from "./SelecTags";
+import { instance } from "../../shared/api";
 import "./style.scss";
+
 
 
 
@@ -7,56 +10,50 @@ import "./style.scss";
 export default function RegionModal({closeModal}) {
 
     const [isChecked, setIsChecked] = useState(false); //체크여부
-    const [checkedItems,setCheckedItems]=useState({tag:""})
+    const [region,setRegion] =useState({name:""});
 
     const regionList = [
-        { id: 1, tag: "전체" },
-        { id: 2, tag: "서울/경기" },
-        { id: 3, tag: "대전" },
-        { id: 4, tag: "세종" },
-        { id: 5, tag: "인천" },
-        { id: 6, tag: "대구" },
-        { id: 7, tag: "강원도" },
-        { id: 8, tag: "울산" },
-        { id: 9, tag: "충청도" },
-        { id: 10, tag: "광주" },
-        { id: 11, tag: "전라도" },
-        { id: 12, tag: "부산" },
-        { id: 13, tag: "경상도" },
-        { id: 14, tag: "제주도" },
+        { name: "전체" },
+        { name: "서울/경기" },
+        { name: "대전" },
+        { name: "세종" },
+        { name: "인천" },
+        { name: "대구" },
+        { name: "강원도" },
+        { name: "울산" },
+        { name: "충청도" },
+        { name: "광주" },
+        { name: "전라도" },
+        { name: "부산" },
+        { name: "경상도" },
+        { name: "제주도" },
       ];
 
-      const changeHandler = (e)=> {
-        const { name, value } = e.target;
-         if(isChecked) {
-            setCheckedItems(checkedItems);
-          } 
-          if (checkedItems.size > 1) {
-            checkedItems.delete();
-            setCheckedItems(checkedItems);
-            e.preventDefault();
-            window.alert("최대 3개까지 선택가능합니다");
-          } else {
-        setIsChecked(!isChecked);
-        setCheckedItems({tag:e.target.value})
-    }
-        };
+    //   console.log(region)
+    // const getRegionList =async () => {
+    //     const response = await instance.post(`/api/post`);
+    //     console.log(response)
+    //     return response.data;
+    //   };
+      
+  
+    // useEffect(()=> {
+    // getRegionList() //dispatch쓰지 않고 불러오는방법^^
+    // },[region]);
+
+    
 
     return (
         <div className="modalBackground">
             <div className="modalContainer">
                 <div className="conStyle">
-            {regionList.map((item) => (
-           <label key={item.tag}>
-             <input
-             type="checkbox"
-             style={{display:"none",}}
-             value={item.tag}
-             onChange={changeHandler}
-            />
-           <div>{item.tag}</div>
-          </label>
-         ))}
+               <SelecTags
+               key={regionList.name}
+               selected={region == regionList.name}
+               handler={() => setRegion(regionList.name)}
+               value={regionList.name}
+            //    onClick={getRegionList}
+               />
         </div>
         <div className="buttonbox">
                     <button className="닫기버튼" onClick={()=> closeModal(false)}>취소</button>
