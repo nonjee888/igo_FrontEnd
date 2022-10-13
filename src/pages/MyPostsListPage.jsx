82a0f8b092e41915ba881652d4626c4a377b872f
dahyun612@naver.com <dahyun612@naver.com>
2022-10-14 00:44:32 +0900

    로그인 필요한 page에 interest choice page useEffect걸어주기

--- src/pages/MyPostsListPage.jsx
+++ src/pages/MyPostsListPage.jsx
@@ -3,8 +3,26 @@ import MyPostsList from "../components/mypage/MyPostsList";
 import Headers from "../components/layout/Headers";
 import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getMyinfo } from "../redux/modules/myinfo";
+
 
 const MyPostsListPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("ACCESS_TOKEN") !== null) {
+      dispatch(getMyinfo()).then((response) => {
+        console.log(response.payload[0].interested);
+        if (response.payload[0].interested === null) {
+          navigate("/choice");
+        }
+      });
+    }
+  });
+
   return (
     <PageContainer>
       <Headers />
--- src/pages/StoryAdd.jsx
+++ src/pages/StoryAdd.jsx
@@ -3,10 +3,26 @@ import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
 import Headers from "../components/layout/Headers";
 import AddStory from "../components/story/AddStory";
-
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getMyinfo } from "../redux/modules/myinfo";
 
 
 const Story = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("ACCESS_TOKEN") !== null) {
+      dispatch(getMyinfo()).then((response) => {
+        console.log(response.payload[0].interested);
+        if (response.payload[0].interested === null) {
+          navigate("/choice");
+        }
+      });
+    }
+  });
+
   return(
     <PageContainer>
       <Headers/>
