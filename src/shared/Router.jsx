b8e26931df049b215b0de8727e1cad8d5084220b
Eunji Roh <nonjee888@gmail.com>
2022-10-17 22:48:11 +0900

    [fix] BrowserRouter App.js로 옮김

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -27,9 +27,8 @@ import { getCookie, setCookie } from "./cookie";
 import { EventSourcePolyfill } from "event-source-polyfill";
 import { instance } from "./api";
 
-
 const Router = () => {
- let eventSource = undefined;
+  let eventSource = undefined;
   let token = localStorage.getItem("ACCESS_TOKEN");
 
   const isSSE = async () => {
@@ -79,7 +78,7 @@ const Router = () => {
           showNotification();
         }
       })();
-    })
+    });
   };
 
   useEffect(() => {
@@ -112,36 +111,32 @@ const Router = () => {
 
   return (
     <div>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/kakaoloading" element={<KaKaoLoading />} exact />
-          <Route path="/naverloading" element={<NaverLoading />} exact />
-          <Route path="/" element={<Loginpage />} exact />
-          <Route path="/login" element={<LoginAdmin />} exact />
-          <Route path="/choice" element={<ChoiceCategory />} exact />
-          <Route path="/recommend" element={<MainRecommend />} exact />
-          <Route path="/post/:category" element={<AllCategoryList />} exact />
-          {/*post/:region에서 앞에 :값을 제거해주니 세부카테고리 확인됨 */}
-          <Route path="/createstory" element={<CreateStory />} exact />
-          <Route path="/search/" element={<SearchPage />} exact />
-          <Route path="/search/:searchTerm" element={<SearchPage />} exact />
-          <Route path="/story" element={<Story />} exact />
-          <Route path="/addstory" element={<StoryAdd />} exact />
-          <Route path="/myinfo" element={<MyInfoPage />} exact />
-          <Route path="/mylike" element={<MyLikesPage />} exact />
-          <Route path="/myplan" element={<MyPlanPage />} exact />
-          <Route path="/myplanpost" element={<MyPlanPostPage />} exact />
-          <Route path="/mypostlist" element={<MyPostsListPage />} exact />
-          <Route path="/notification" element={<Notice />} exact />
-          <Route path="*" element={<div>없는 페이지입니다.</div>} />
-          <Route path="/addpost" element={<AddPostPage />} exact />
-          <Route path="/addpost/edit/:id" element={<AddPostPage />} exact />
-          <Route path="/postdetail/:id" element={<PostDetailPage />} exact />
-          <Route path="/tutorial" element={<Tutorial />} exact />
-        </Routes>
-      </BrowserRouter>
-
+      <Routes>
+        <Route path="/kakaoloading" element={<KaKaoLoading />} exact />
+        <Route path="/naverloading" element={<NaverLoading />} exact />
+        <Route path="/" element={<Loginpage />} exact />
+        <Route path="/login" element={<LoginAdmin />} exact />
+        <Route path="/choice" element={<ChoiceCategory />} exact />
+        <Route path="/recommend" element={<MainRecommend />} exact />
+        <Route path="/post/:category" element={<AllCategoryList />} exact />
+        {/*post/:region에서 앞에 :값을 제거해주니 세부카테고리 확인됨 */}
+        <Route path="/createstory" element={<CreateStory />} exact />
+        <Route path="/search/" element={<SearchPage />} exact />
+        <Route path="/search/:searchTerm" element={<SearchPage />} exact />
+        <Route path="/story" element={<Story />} exact />
+        <Route path="/addstory" element={<StoryAdd />} exact />
+        <Route path="/myinfo" element={<MyInfoPage />} exact />
+        <Route path="/mylike" element={<MyLikesPage />} exact />
+        <Route path="/myplan" element={<MyPlanPage />} exact />
+        <Route path="/myplanpost" element={<MyPlanPostPage />} exact />
+        <Route path="/mypostlist" element={<MyPostsListPage />} exact />
+        <Route path="/notification" element={<Notice />} exact />
+        <Route path="*" element={<div>없는 페이지입니다.</div>} />
+        <Route path="/addpost" element={<AddPostPage />} exact />
+        <Route path="/addpost/edit/:id" element={<AddPostPage />} exact />
+        <Route path="/postdetail/:id" element={<PostDetailPage />} exact />
+        <Route path="/tutorial" element={<Tutorial />} exact />
+      </Routes>
     </div>
   );
 };
