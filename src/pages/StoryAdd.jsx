38f14ab2dc9cf11090dad9ac78c774097b42d354
SHL <s2hlimi@yahoo.co.jp>
2022-10-18 11:20:33 +0900

    [refactor]

--- src/pages/StoryAdd.jsx
+++ src/pages/StoryAdd.jsx
@@ -3,23 +3,23 @@ import Footers from "../components/layout/Footers";
 import PageContainer from "../components/wrapper/PageContainer";
 import Headers from "../components/layout/Headers";
 import AddStory from "../components/story/AddStory";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { useEffect } from "react";
-// import { getMyinfo } from "../redux/modules/myinfo";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getMyinfo } from "../redux/modules/myinfo";
 
 const Story = () => {
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   if (localStorage.getItem("ACCESS_TOKEN") !== null) {
-  //     dispatch(getMyinfo()).then((response) => {
-  //       if (response.payload[0].interested === null) {
-  //         navigate("/choice");
-  //       }
-  //     });
-  //   }
-  // });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("ACCESS_TOKEN") !== null) {
+      dispatch(getMyinfo()).then((response) => {
+        if (response.payload[0].interested === null) {
+          navigate("/choice");
+        }
+      });
+    }
+  });
 
   return (
     <PageContainer>
