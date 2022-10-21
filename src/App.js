520c324f706425dd5760f81db773bc4ac42bed94
dahyun612@naver.com <dahyun612@naver.com>
2022-10-21 15:50:48 +0900

    [feat]웹 전용백그라운드 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import "./style.scss";
 import Router from "./shared/Router";
 import { BrowserRouter } from "react-router-dom";
+import webImg from "./asset/backImg.png";
 
 function App() {
   return (
     <>
+    
       <BrowserRouter>
+      <img className="webImg" src={webImg}/>
         <div className="mobile-wrapper">
           <Router />
         </div>
       </BrowserRouter>
+     
     </>
   );
 }
