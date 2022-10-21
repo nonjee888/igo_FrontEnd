7c77cbf5ffadc3deb456576c8f3c246e432e0301
Eunji Roh <nonjee888@gmail.com>
2022-10-21 21:12:22 +0900

    [remove] MyNotification.jsx 삭제, 모달창으로 옮김

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -15,7 +15,6 @@ import MyLikesPage from "../pages/MyLikesPage";
 import MyPlanPage from "../pages/MyPlanPage";
 import MyPlanPostPage from "../pages/MyPlanPostPage";
 import MyPostsListPage from "../pages/MyPostsListPage";
-import Notice from "../pages/Notice";
 import StoryAdd from "../pages/StoryAdd";
 import AllCategoryList from "../components/category/AllCategoryList";
 import Tutorial from "../components/tutorial/Tutorial";
@@ -129,7 +128,6 @@ const Router = () => {
         <Route path="/myplan" element={<MyPlanPage />} exact />
         <Route path="/myplanpost" element={<MyPlanPostPage />} exact />
         <Route path="/mypostlist" element={<MyPostsListPage />} exact />
-        <Route path="/notification" element={<Notice />} exact />
         <Route path="*" element={<div>없는 페이지입니다.</div>} />
         <Route path="/addpost" element={<AddPostPage />} exact />
         <Route path="/addpost/edit/:id" element={<AddPostPage />} exact />
