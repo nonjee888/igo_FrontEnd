f7f1f75a8235ac843981581b08f2604b94533ed7
Eunji Roh <nonjee888@gmail.com>
2022-10-24 13:49:01 +0900

    [fix] web용 배경화면 사이즈조절

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import "./App.css";
 import "./style.scss";
 import Router from "./shared/Router";
 import { BrowserRouter } from "react-router-dom";
-import webImg from "./asset/backImg.png";
+import background from "./asset/background.png";
 
 function App() {
   return (
     <>
-    
       <BrowserRouter>
-      <img className="webImg" src={webImg}/>
+        <img className="webImg" src={background} />
         <div className="mobile-wrapper">
           <Router />
         </div>
       </BrowserRouter>
-     
     </>
   );
 }
